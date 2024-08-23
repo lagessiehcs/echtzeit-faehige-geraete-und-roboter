2005ae6065cdbbfe7a4b4df8ac2698fd9976d529
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-08-23 19:53:44 +0200

    Aufgabe 1: erster Versuch

--- T1-Aufgabe1/src/main.c
+++ T1-Aufgabe1/src/main.c
@@ -9,6 +9,27 @@ int main(void)
 {
     // place code to run once here
 
+    // ------- STEP 1: Module aktivieren -------
+    // Aktivierung des GPIO A Moduls
+    uint32_t volatile *adresse = (uint32_t *)(0x40021000 + 0x4C);
+    *adresse |= (1 << 0);
+
+    // ------- STEP 2: Pin als Output konfigurieren -------
+    // LED Pin als Output konfigurieren
+    adresse = (uint32_t *)(0x48000000 + 0x00);
+    *adresse &= ~(1 << 17);
+    *adresse |= (1 << 16);
+
+    // LED Pin als push-pull einstellen
+    adresse = (uint32_t *)(0x48000000 + 0x04);
+    *adresse &= ~(1 << 8);
+
+    // ------- STEP 3: Pin einschalten -------
+    // LED anschalten
+    adresse = (uint32_t *)(0x48000000 + 0x14);
+    *adresse |= (1 << 8);
+
+
     while (1)
     {
         // place your loop code here
