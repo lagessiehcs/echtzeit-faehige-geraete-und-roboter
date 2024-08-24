fad02e1d88fa38a10f143398e21b0f22332399c8
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-08-24 09:14:32 +0200

    Aufgabe 4: erster Versuch

---
+++ T1-Aufgabe4/lib/GPIO/gpio.c
@@ -0,0 +1,56 @@
+#include "gpio.h"
+void GPIO_Init_Input(uint32_t port, uint8_t pin, uint8_t pupd)
+{
+    GPIO_SetPinMode(port, pin, GPIO_MODE_INPUT);
+    GPIO_SetPinPullUpPullDown(port, pin, pupd);
+}
+void GPIO_Init_Output(uint32_t port, uint8_t pin, uint8_t type)
+{
+    GPIO_SetPinMode(port, pin, GPIO_MODE_OUTPUT);
+    GPIO_SetPinOutputType(port, pin, type);
+}
+void GPIO_SetPinMode(uint32_t port, uint8_t pin, uint8_t mode)
+{
+    GPIO_MODER(port) &= ~(0x3 << (pin * 2)); // Lösche die bestehenden Bits
+    GPIO_MODER(port) |= (mode << (pin * 2)); // Setze den neuen Modus
+}
+
+void GPIO_SetPinOutputType(uint32_t port, uint8_t pin, uint8_t type)
+{
+    GPIO_OTYPER(port) &= ~(0x1 << pin); // Lösche das bestehende Bit
+    GPIO_OTYPER(port) |= (type << pin); // Setze den neuen Typ
+}
+
+void GPIO_SetPinSpeed(uint32_t port, uint8_t pin, uint8_t speed)
+{
+    GPIO_OSPEEDR(port) &= ~(0x3 << (pin * 2));  // Lösche die bestehenden Bits
+    GPIO_OSPEEDR(port) |= (speed << (pin * 2)); // Setze die neue Geschwindigkeit
+}
+
+void GPIO_SetPinPullUpPullDown(uint32_t port, uint8_t pin, uint8_t pupd)
+{
+    GPIO_PUPDR(port) &= ~(0x3 << (pin * 2)); // Lösche die bestehenden Bits
+    GPIO_PUPDR(port) |= (pupd << (pin * 2)); // Setze die neue Pull-Up/Pull-Down Konfiguration
+}
+
+void GPIO_WritePin(uint32_t port, uint8_t pin, uint8_t state)
+{
+    if (state)
+    {
+        GPIO_BSRR(port) = (1 << pin); // Setze den Pin
+    }
+    else
+    {
+        GPIO_BRR(port) = (1 << pin); // Lösche den Pin
+    }
+}
+
+uint8_t GPIO_ReadPin(uint32_t port, uint8_t pin)
+{
+    return (GPIO_IDR(port) & (1 << pin)) ? 1 : 0; // Lese den Pin-Zustand
+}
+
+void GPIO_TogglePin(uint32_t port, uint8_t pin)
+{
+    GPIO_ODR(port) ^= (1 << pin); // Togglen des Pin-Zustands
+}
--- T1-Aufgabe4/src/main.c
+++ T1-Aufgabe4/src/main.c
@@ -4,15 +4,84 @@
  */
 
 #include <stdint.h>
+#include <gpio.h>
+#include <Delay_ms.h>
+
+#define BLUE 1
+#define RED 2
+#define GREEN 3
+
+#define LED_BLUE GPIOA_BASE, 8
+#define LED_RED GPIOA_BASE, 9
+#define LED_GREEN GPIOA_BASE, 10
+
+#define BUTTON1 GPIOA_BASE, 2
+#define BUTTON2 GPIOA_BASE, 3
+
+uint32_t milliseconds = 500;
+int color_change_on = 1;
+int current_color = BLUE;
+
+void change_color_LED(uint32_t port, uint8_t pin);
+int is_button_pushed(uint32_t port, uint8_t pin);
 
 int main(void)
 {
     // place code to run once here
+    GPIO_Init_Output(LED_BLUE, GPIO_OTYPE_PP);
+    GPIO_Init_Output(LED_RED, GPIO_OTYPE_PP);
+    GPIO_Init_Output(LED_GREEN, GPIO_OTYPE_PP);
+
+    GPIO_Init_Input(BUTTON1, GPIO_PU);
+    GPIO_Init_Input(BUTTON2, GPIO_PU);
 
     while (1)
     {
         // place your loop code here
+
+        if (is_button_pushed(BUTTON1))
+        {
+            color_change_on = !color_change_on;
+        }
+
+        if (is_button_pushed(BUTTON2))
+        {
+            milliseconds -= 10;
+            if (milliseconds < 0)
+            {
+                milliseconds = 0;
+            }
+        }
+
+        if (color_change_on)
+        {
+            change_color_LED(LED_BLUE);
+        }
     }
 
     return 0;
+}
+
+void change_color_LED(uint32_t port, uint8_t pin)
+{
+    if (current_color == BLUE)
+    {
+        GPIO_TogglePin(LED_RED);
+        current_color = RED;
+    }
+    else if (current_color == RED)
+    {
+        GPIO_TogglePin(LED_GREEN);
+        current_color = GREEN;
+    }
+    else if (current_color == GREEN)
+    {
+        GPIO_TogglePin(LED_BLUE);
+        current_color = BLUE;
+    }
+    delay_ms(milliseconds);
+}
+int is_button_pushed(uint32_t port, uint8_t pin)
+{
+    return GPIO_ReadPin(port, pin);
 }
